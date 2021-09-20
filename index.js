7dbc6d64073004a688756b272f86a3c4b6f23b4e
NiftyAndy <andy.niftyleague@gmail.com>
2021-09-20 03:03:37 -0700

    Set API to forward metadata from s3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const fs = require('fs');
+const fetch = require('node-fetch');
 const cors = require('cors');
 const path = require('path');
 
@@ -20,26 +20,30 @@ app.get('/', function (req, res) {
   res.send('OpenSea API for the Nifty League!');
 });
 
-function resolveMetadata(req) {
+async function resolveMetadata(req) {
   const targetNetwork = req.params.network;
   const tokenId = parseInt(req.params.token_id).toString();
-  const metadataPath = `./metadata/${targetNetwork}/${tokenId}.json`;
-  return fs.existsSync(metadataPath)
-    ? JSON.parse(fs.readFileSync(metadataPath, { encoding: 'utf8' }))
-    : {};
+  const metadataURI = `https://nifty-league.s3.amazonaws.com/degens/${targetNetwork}/metadata/${tokenId}.json`;
+  const response = await fetch(metadataURI);
+  return response.json();
 }
 
-app.get('/:network/degen/:token_id', function (req, res) {
-  const metadata = resolveMetadata(req);
+app.get('/:network/degen/:token_id', async function (req, res) {
+  const metadata = await resolveMetadata(req);
   res.send(metadata);
 });
 
-app.get('/:network/degen/:token_id/rarity', function (req, res) {
-  const metadata = resolveMetadata(req);
+app.get('/:network/degen/:token_id/rarity', async function (req, res) {
+  const metadata = await resolveMetadata(req);
   const rarity = metadata.attributes?.find(a => a.trait_type === 'Rarity');
   if (rarity) res.send(rarity.value);
   else res.sendStatus(404);
 });
+
+// app.post('/:network/webhooks/degen', function (req, res) {
+//   const targetNetwork = req.params.network;
+
+// });
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
